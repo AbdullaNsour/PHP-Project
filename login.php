fca9879c0b01655d6c6869611954d11b4c9ce92c
AbdullaNsour <alnsour.194@gmial.com>
2022-09-30 14:05:18 +0300

    change colors of success form rid to green

--- login.php
+++ login.php
@@ -1,78 +1,59 @@
 <?php
+
+include 'config.php';
 session_start();
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db_name = "e-commerce";
-// Create connection
-$conn = new mysqli($servername, $username, $password, $db_name);
+if(isset($_POST['submit'])){
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
+
+   $select = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email' AND password = '$pass'") or die('query failed');
+
+   if(mysqli_num_rows($select) > 0){
+      $row = mysqli_fetch_assoc($select);
+      $_SESSION['id'] = $row['id'];
+      header('location:home.php');
+   }else{
+      $message[] = 'incorrect email or password!';
+   }
+
 }
 
-if (isset($_POST['submit'])) {
-
-    $uname = $_POST['email'];
-    $password = $_POST['password'];
-
-    $sql = "SELECT * FROM user WHERE email='$uname' AND password='$password'";
-    $result = mysqli_query($conn, $sql);
-    $row = mysqli_fetch_assoc($result);
-
-
-    if ($row['email'] == $uname && $row['password'] == $password && $row['role'] == 'admin') {
-        echo "Logged in!<br>";
-        $_SESSION['first_name'] = $row['first_name'];
-        $_SESSION['id'] = $row['id'];
-
-        header("location:welcome.php");
-    } elseif ($row['email'] == $uname && $row['password'] == $password && $row['role'] == 'user') {
-        echo "Logged in!<br>";
-        $_SESSION['first_name'] = $row['first_name'];
-        $_SESSION['id'] = $row['id'];
-        $_SESSION['first_name'] = $row['first_name'];
-        $_SESSION['last_name'] = $row['last_name'];
-        header("location:welcome.php");
-    }
-}
 ?>
 
-
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title> Login</title>
-    <link rel="stylesheet" href="style.css">
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>login</title>
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
 </head>
-
 <body>
-    <center>
-        <h1>login </h1>
-    </center>
-    <center>
-        <h5>welcome back</h5>
-    </center>
-    <div class="container">
+   
+<div class="form-container">
 
-        <center>
-            <form method="POST">
-                <div class="email">
+   <form action="" method="post" enctype="multipart/form-data">
+      <h3>login now</h3>
+      <?php
+      if(isset($message)){
+         foreach($message as $message){
+            echo '<div class="message">'.$message.'</div>';
+         }
+      }
+      ?>
+      <input type="email" name="email" placeholder="enter email" class="box" required>
+      <input type="password" name="password" placeholder="enter password" class="box" required>
+      <input type="submit" name="submit" value="login now" class="btn">
+      <p>don't have an account? <a href="register.php">regiser now</a></p>
+   </form>
 
-                    <input type="email" name="email" placeholder="Enter the User email" required />
-                </div>
-                <div class="pass">
+</div>
 
-                    <input type="password" name="password" placeholder="password" required>
-                </div>
-                <p class="text-center mt-2 fs-2 text-muted">Don't have an account?<a href="signup.php">sign up</a></p>
-                <input type="submit" name="submit" type="submit" value="LOGIN" class="btn" />
-            </form>
-        </center>
-    </div>
 </body>
-
 </html>
